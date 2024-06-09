a430cca3e632fe6d7d3e74d9f4e0bb65199e986c
Hytálo Benaventana <pd_troia@hotmail.com>
2024-06-09 17:53:24 -0300

    first-commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod pure_split;
+fn main() {
+    let str = String::from("maria antonieta de lima");
+    let char = ' ';
+    let vec = pure_split::pure_split(&str,char);
+    println!("{}",vec.join(", "));
+}
---
+++ src/pure_split/mod.rs
@@ -0,0 +1,18 @@
+use std::usize;
+
+pub fn pure_split(str:&String,ch:char)->Vec<&str>{
+    let mut i = 0;
+    let mut j = 0;
+    let mut splited : Vec<&str> = Vec::new();
+    for (idx,byte) in str.bytes().enumerate(){
+        if idx == str.len()-1{
+            splited.push(&str[j..]);
+        }
+        if byte == ch as u8 {
+            splited.push(&str[j..i]);
+            j = i + 1 
+        }
+        i = i + 1;
+    }
+    splited
+}
